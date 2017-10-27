505800b8fd52b835e5ac516b392b0d63e265b97c
Andrey Khorkov <and.khorkov@gmail.com>
2017-10-28 03:14:20 +0500

    what i have so far

---
+++ Assets/Scripts/UI/Btns/Test.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private Animator animator;
+    private int setView2Id = Animator.StringToHash("SetView2");
+
+    public enum Views
+    {
+        view1,
+        view2
+    }
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            animator.SetTrigger(setView2Id);
+        }
+
+        if (Input.GetKey(KeyCode.B))
+        {
+            animator.SetTrigger("SetView1");
+        }
+    }
+}
